b13acc009b099349a0d5edee572054b4430ccdea
love <hoangtien96da@gmail.com>
2023-03-04 06:51:10 +0700

    them

---
+++ lesson8_ham/TH_ham02.php
@@ -0,0 +1,10 @@
+<?php
+function reverseString ($str){
+    $result = "";
+    for($i = strlen ($str)-1; $i >= 0; $i--){
+        $result = $str[$i];
+    }
+    return $result;
+}
+echo reverseString ("Hello World!");
+?>
